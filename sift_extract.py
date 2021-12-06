1080601d0eb01a233c349067caee7ebf250552c1
Long Ho <lhho@princeton.edu>
2021-12-06 15:39:15 -0500

    nearest_neighbor_classifier

---
+++ sift_extract.py
@@ -0,0 +1,42 @@
+
+import numpy as np
+import cv2
+import matplotlib.pyplot as plt
+
+
+############## SIFT ############################################################
+
+def extract_sift(img, step_size=1):
+    """
+    Extract SIFT features for a given grayscale image.
+    Feel free to use OpenCV functions.
+    Args:
+        img: Grayscale image of shape (H, W)
+        step_size: Size of the step between keypoints
+    """
+    sift = cv2.SIFT_create()
+
+
+    keypoints = [cv2.KeyPoint(j, i, step_size) for i in range(0, img.shape[0], step_size) for j in range(0, img.shape[1], step_size)]
+
+    _, descriptors = sift.compute(img, keypoints)
+
+    return descriptors
+
+
+def extract_sift_for_dataset(data, step_size=1):
+
+    num_clusters = data.shape[0]
+    num_examples = data.shape[1]
+
+    all_features = []
+
+    for i in range(num_clusters):
+        curr_cluster = []
+        for j in range(num_examples):
+            img = data[i][j]
+            img = cv2.cvtColor(np.uint8(img), cv2.COLOR_BGR2GRAY)
+            descriptors = extract_sift(img, step_size)
+            curr_cluster.append(descriptors)
+        all_features.append(curr_cluster)
+    return all_features
