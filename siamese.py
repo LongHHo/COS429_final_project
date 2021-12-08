ba1384ea0200c2719f8872611f74dd06edf94213
Long Ho <lhho@princeton.edu>
2021-12-08 18:11:20 -0500

    starting siamese stuff

---
+++ siamese.py
@@ -0,0 +1,178 @@
+from imutils import build_montages
+import numpy as np
+import cv2
+
+
+import matplotlib.pyplot as plt
+import os
+import random
+import tensorflow as tf
+from pathlib import Path
+from tensorflow.keras import applications
+from tensorflow.keras import layers
+from tensorflow.keras import losses
+from tensorflow.keras import optimizers
+from tensorflow.keras import metrics
+from tensorflow.keras import Model
+from tensorflow.keras.applications import resnet
+
+def get_pairwise_batch(batch_size, train_data):
+    """
+    Create batch of n pairs, half same class, half different class
+    """
+    n_classes, n_examples, w, h, d = train_data.shape
+    
+
+    rng = np.random.default_rng()
+
+    # randomly sample several classes to use in the batch
+    categories = rng.choice(n_classes,size=(batch_size,),replace=False)
+    
+    # initialize 2 empty arrays for the input image batch
+    pairs=[np.zeros((batch_size, h, w, d)) for i in range(2)]
+    
+    # initialize vector for the targets
+    targets=np.zeros((batch_size,))
+    
+    # make one half of it '1's, so 2nd half of batch has same class
+    targets[batch_size//2:] = 1
+    for i in range(batch_size):
+        category = categories[i]
+        idx_1 = rng.randint(0, n_examples)
+        pairs[0][i,:,:,:] = train_data[category, idx_1].reshape(w, h, d)
+        idx_2 = rng.randint(0, n_examples)
+        
+        # pick images of same class for 1st half, different for 2nd
+        if i >= batch_size // 2:
+            category_2 = category  
+        else: 
+            # add a random number to the category modulo n classes to ensure 2nd image has a different category
+            category_2 = (category + rng.randint(1,n_classes)) % n_classes
+        
+        pairs[1][i,:,:,:] = train_data[category_2,idx_2].reshape(w, h, d)
+    
+    return pairs, targets
+
+
+# returns three lists: anchor, positive, negative
+def get_triple_batch(batch_size, train_data):
+    """
+    Create three lists of anchor images, positive images, negative images
+    """
+    n_classes, n_examples, w, h, d = train_data.shape
+    
+
+    rng = np.random.default_rng()
+
+    # randomly sample several classes to use in the batch
+    categories = rng.choice(n_classes,size=(batch_size,),replace=False)
+    
+    # initialize 2 empty arrays for the input image batch
+    anchor = np.zeros((batch_size, h, w, d))
+
+    positive = np.zeros((batch_size, h, w, d))
+
+    negative = np.zeros((batch_size, h, w, d))
+
+    
+    for i in range(batch_size):
+        category = categories[i]
+        idx_1 = rng.randint(0, n_examples)
+        anchor[i,:,:,:] = train_data[category, idx_1].reshape(w, h, d)
+
+        
+        idx_pos = rng.randint(0, n_examples)
+        cat_pos = category
+        positive[i,:,:,:] = train_data[cat_pos,idx_pos].reshape(w, h, d)
+
+        idx_neg = rng.randint(0, n_examples)
+        cat_neg = (category + rng.randint(1,n_classes)) % n_classes
+        negative[i,:,:,:] = train_data[cat_neg,idx_neg].reshape(w, h, d)
+
+    return anchor, positive, negative
+
+
+class DistanceLayer(layers.Layer):
+    """
+    This layer is responsible for computing the distance between the anchor
+    embedding and the positive embedding, and the anchor embedding and the
+    negative embedding.
+    """
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+
+    def call(self, anchor, positive, negative):
+        ap_distance = tf.reduce_sum(tf.square(anchor - positive), -1)
+        an_distance = tf.reduce_sum(tf.square(anchor - negative), -1)
+        return (ap_distance, an_distance)
+    
+
+
+
+
+
+class SiameseModel(Model):
+    """The Siamese Network model with a custom training and testing loops.
+
+    Computes the triplet loss using the three embeddings produced by the
+    Siamese Network.
+
+    The triplet loss is defined as:
+       L(A, P, N) = max(‖f(A) - f(P)‖² - ‖f(A) - f(N)‖² + margin, 0)
+    """
+
+    def __init__(self, siamese_network, margin=0.5):
+        super(SiameseModel, self).__init__()
+        self.siamese_network = siamese_network
+        self.margin = margin
+        self.loss_tracker = metrics.Mean(name="loss")
+
+    def call(self, inputs):
+        return self.siamese_network(inputs)
+
+    def train_step(self, data):
+        # GradientTape is a context manager that records every operation that
+        # you do inside. We are using it here to compute the loss so we can get
+        # the gradients and apply them using the optimizer specified in
+        # `compile()`.
+        with tf.GradientTape() as tape:
+            loss = self._compute_loss(data)
+
+        # Storing the gradients of the loss function with respect to the
+        # weights/parameters.
+        gradients = tape.gradient(loss, self.siamese_network.trainable_weights)
+
+        # Applying the gradients on the model using the specified optimizer
+        self.optimizer.apply_gradients(
+            zip(gradients, self.siamese_network.trainable_weights)
+        )
+
+        # Let's update and return the training loss metric.
+        self.loss_tracker.update_state(loss)
+        return {"loss": self.loss_tracker.result()}
+
+    def test_step(self, data):
+        loss = self._compute_loss(data)
+
+        # Let's update and return the loss metric.
+        self.loss_tracker.update_state(loss)
+        return {"loss": self.loss_tracker.result()}
+
+    def _compute_loss(self, data):
+        # The output of the network is a tuple containing the distances
+        # between the anchor and the positive example, and the anchor and
+        # the negative example.
+        ap_distance, an_distance = self.siamese_network(data)
+
+        # Computing the Triplet Loss by subtracting both distances and
+        # making sure we don't get a negative value.
+        loss = ap_distance - an_distance
+        loss = tf.maximum(loss + self.margin, 0.0)
+        return loss
+
+    @property
+    def metrics(self):
+        # We need to list our metrics here so the `reset_states()` can be
+        # called automatically.
+        return [self.loss_tracker]
