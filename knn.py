450b03c0ed48427119cef3029dbf4fcdb66d45ae
Jackson Deitelzweig <jackson.deitelzweig@gmail.com>
2021-12-13 04:22:30 -0500

    Fix tensorboard import

--- knn.py
+++ knn.py
@@ -3,7 +3,7 @@ from typing import List
 import numpy as np
 from scipy.spatial.distance import cdist
 import tensorflow as tf
-from tensorflow.contrib.tensorboard.plugins import projector
+from tensorboard.plugins import projector
 
 def kNN(support_feats: np.ndarray, support_labels: List[int], example_feat: np.ndarray, k: int = 1, metric: str = 'euclidean') -> int:
     distances = cdist(support_feats, [example_feat], metric=metric).reshape(-1)
@@ -29,13 +29,13 @@ def convert_embeddings_to_tf(embeddings: np.ndarray, labels: np.ndarray, log_dir
     embed.metadata_path = 'metadata.tsv'
 
     # define the model without training
-    sess = tf.InteractiveSession()
+    sess = tf.compat.v1.InteractiveSession()
     
-    tf.global_variables_initializer().run()
-    saver = tf.train.Saver()
+    tf.compat.v1.global_variables_initializer().run()
+    saver = tf.compat.v1.train.Saver()
     
     saver.save(sess, os.path.join(log_dir, 'embeddings.ckpt'))
 
-    writer = tf.summary.FileWriter(log_dir, sess.graph)
+    writer = tf.compat.v1.summary.FileWriter(log_dir, sess.graph)
     projector.visualize_embeddings(writer, config)
     sess.close()
